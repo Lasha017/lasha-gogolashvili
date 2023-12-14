83b23ecfb708954592049bb664d3b82138c36435
Lasha Gogolashvili <lashagogolashvili@Lashas-MacBook-Air.local>
2023-12-14 19:05:07 +0400

    js-homework

---
+++ new-js/file.js
@@ -0,0 +1,35 @@
+const add = document.querySelector('.btn');
+const ul = document.querySelector('ul');
+
+add.addEventListener('click', () => {
+    const input = document.querySelector('input');
+    input.classList.add('input-1')
+    const li = document.createElement('li');
+    li.innerText = input.value;
+    ul.appendChild(li);
+    console.log(li);
+    
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox'
+    checkbox.classList.add('input-2');
+    li.appendChild(checkbox);
+    checkbox.style.color = 'red';
+    checkbox.style.borderRadius = '20px';
+    console.log(li);
+    
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'delete';
+    deleteBtn.classList.add('delete-btn');
+    li.appendChild(deleteBtn);
+    deleteBtn.src = 'trash.svg';
+    deleteBtn.style.backgroundColor = 'white';
+    console.log(li);
+
+
+    const icon = document.createElement('img');
+    icon.src = 'trash.svg'; 
+    icon.alt = 'Delete Icon'; 
+    deleteBtn.appendChild(icon);
+    deleteBtn.style.backgroundColor = 'white'; 
+    console.log(li);
+});
