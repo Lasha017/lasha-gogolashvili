7925a5e9e8efb0cff8249e0c053b2749275d0eb8
Lasha Gogolashvili <lashagogolashvili@Lashas-MacBook-Air.local>
2024-01-11 19:26:28 +0400

    weather-website

---
+++ Weather-Website/second-page/secondfile.js
@@ -0,0 +1,10 @@
+function myfunction() {
+    const element = document.body;
+    element.classList.toggle('light-mode');
+}
+function toggleCollapsible() {
+    var collapsibleContent = document.querySelector('.collapsible-content');
+    collapsibleContent.classList.toggle('show');    
+}
+document.querySelector('.collapsible').addEventListener('click', toggleCollapsible);
+    
